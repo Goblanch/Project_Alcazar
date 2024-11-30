db96bc6ab2f3f06184cd792133a4b72aca04af80
GoBlanch <gonzaloblanchdev@gmail.com>
2024-11-30 19:14:11 +0100

    Refactor ClickableBase class. Changed to an abstract class

--- Assets/Scripts/Clickables/ClickableBase.cs
+++ Assets/Scripts/Clickables/ClickableBase.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-public class ClickableBase : MonoBehaviour, IClickable
+public abstract class ClickableBase : MonoBehaviour, IClickable
 {
     public ClickableData itemData;
 
-    private bool _isBeingHovered;
+    protected bool _isBeingHovered;
     
 
 #region ICLICKABLE IMPLEMENTATION
@@ -16,34 +16,60 @@ public class ClickableBase : MonoBehaviour, IClickable
     }
 
     void IClickable.OnHoverStarted(){
+        OnHoverStart();
+    }
+
+    void IClickable.OnHover(){
+        OnHover();        
+    }
+
+    void IClickable.OnHoverEnded(){
+        OnHoverEnded();
+    }
+
+    void IClickable.OnHold(Vector2 position){
+        OnHold();
+    }
+
+#endregion
+
+    /// <summary>
+    /// Called once when mouse start hovering the clickable object
+    /// </summary>
+    protected virtual void OnHoverStart(){
         UIController.ContextMenuStartEvent?.Invoke();
         UIController.ContextMenuDataEvent?.Invoke(itemData);
     }
 
-    void IClickable.OnHover()
-    {
+    /// <summary>
+    /// Called every frame mouse is hovering the clickable object
+    /// </summary>
+    protected virtual void OnHover(){
         _isBeingHovered = true;
         // Notify UI to show context menu with info
         UIController.ContextMenuEvent?.Invoke();
     }
 
-    public void OnHoverEnded()
-    {
+    /// <summary>
+    /// Called once when mouse ends hovering the clickable object
+    /// </summary>
+    protected virtual void OnHoverEnded(){
         if(_isBeingHovered){
             _isBeingHovered = false;
             UIController.ContextMenuEndEvent?.Invoke();
         }
     }
 
-    void IClickable.OnHold(Vector2 position){
+    /// <summary>
+    /// Called every frame meanwhile mouse is holding click over the clickable object
+    /// </summary>
+    protected virtual void OnHold(){
         //transform.position = new Vector3(position.x, position.y, transform.position.z);
         // TODO: BUG when holded less than holdThreshold, hold keeps executing when it should be cancelled
         Debug.Log("Holding");
     }
 
-#endregion
-
-    private void DEBUG_LogItemInfo(){
+    protected void DEBUG_LogItemInfo(){
         Debug.Log("ITEM NAME: " + itemData.name);
         Debug.Log("ITEM DESCRIPTION: " + itemData.itemDescription);
         Debug.Log("ITEM EFFECT: " + itemData.itemEffect.ToString());
---
+++ Assets/Scripts/Clickables/ItemPickable.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ItemPickable : ClickableBase, IClickable
+{
+    protected override void OnHoverStart()
+    {
+        base.OnHoverStart();
+    }
+
+    protected override void OnHover()
+    {
+        base.OnHover();
+    }
+
+    protected override void OnHoverEnded()
+    {
+        base.OnHoverEnded();
+    }
+
+    protected override void OnHold()
+    {
+        base.OnHold();
+    }
+}
